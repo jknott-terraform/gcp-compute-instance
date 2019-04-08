533ac46cc7a65c271c18e602af2c9ebfd132397f
jknott-terraform <49401490+jknott-terraform@users.noreply.github.com>
2019-04-08 12:36:23 -0500

    added startup script

--- compute.tf
+++ compute.tf
@@ -3,20 +3,35 @@ resource "google_compute_instance" "ubuntu-xenial" {
    name = "ubuntu-xenial"
    machine_type = "f1-micro"
    zone = "us-central1-a"
-   tags = [
+     tags = [
     "${var.subnet}-firewall-ssh",
     "${var.subnet}-firewall-http",
     "${var.subnet}-firewall-https",
     "${var.subnet}-firewall-icmp",
   ]
+  metadata {
+        startup-script = <<SCRIPT
+        apt-get -y update
+        apt-get -y upgrade
+        export HOSTNAME=$(hostname | tr -d '\n')
+        export PRIVATE_IP=$(curl -sf -H 'Metadata-Flavor:Google' http://metadata/computeMetadata/v1/instance/network-interfaces/0/ip | tr -d '\n')
+        echo $HOSTNAME
+        echo $PRIVATE_IP
+        SCRIPT
+    }
    boot_disk {
       initialize_params {
       image = "ubuntu-1604-lts"
    }
+
+
+
+
+
 }
 network_interface {
-   subnetwork = "development-subnet" # To drop the instance in a specific subnet
-   # network = "default" - Default VPC
+   subnetwork = "development-subnet"
+   # network = "default"
    access_config {}
 }
 service_account {
